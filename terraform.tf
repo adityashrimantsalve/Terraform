40858f54206783ef188bc0c525b1abe6a7e7573c
Aditya Salve <142791096+adityashrimantsalve@users.noreply.github.com>
2024-04-16 19:40:40 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,25 @@
+erraform {
+  required_providers {
+    linux = {
+      source = "mavidser/linux"
+      version = "1.0.2"
+    }
+  }
+}
+
+provider "linux" {
+  host = "localhost"
+  user = "root"
+}
+
+resource "linux_user" "testuser" {
+  name = "testuser"
+  uid = 1024
+}
+
+resource "linux_file" "testfile" {
+  path = "/etc/testfile"
+  content = "testcontent"
+  owner = "${linux_user.testuser.name}:${linux_user.testuser.name}"
+  permissions = 777
+}
